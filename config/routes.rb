623e5343635e7fa3119cbd389b9f1fa280771925
jeesong <jeewsong@gmail.com>
2015-08-02 15:51:10 -0700

    added task model and routing, plus basic bootstrap stlying

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  resources :tasks, except: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
